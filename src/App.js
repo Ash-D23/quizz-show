8b5ac68158f6358476b77a0bb322f708bdc6e8fa
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-03-01 21:24:12 +0530

    feat: added login and sign up pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
 import './App.css'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <SignUp />
       <Footer />
     </div>
   );
