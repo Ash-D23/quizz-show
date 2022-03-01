4b3709ffe6e39d3eb53cbc1bc4d60ab89d37e802
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-03-01 12:18:35 +0530

    feat: added homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import HomePage from './Pages/HomePage/HomePage';
 import './App.css'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <HomePage />
       <Footer />
     </div>
   );
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from 'react';
+import './HomePage.css'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <>
+      <div class="container--50 container__flex--center padding--medium ">
+        <div class="about__description padding--medium">
+            <h2 class="text--light text--center margin-bottom--large">Welcome to QuizzShow</h2>
+            <p class='text--large text--center margin-bottom--medium'> 
+                Let's Begin the show
+            </p>
+            <div class="container__flex--center margin-tb--large">
+                <a href="Pages/quizcategory.html"><button class="btn btn--outline">Explore</button></a>
+            </div>
+        </div>
+      </div>
+      <div class="category--container quiz__category">
+        <div class="container__flex--center margin-bottom--large padding--medium">
+            <h2>Category</h2>
+        </div>
+        <div class="container__flex--center container__flex--wrap">
+            <div class="category--card margin--medium">
+                <img src="Images/quiz.jpg" />
+                <h3 class="category--title">Category</h3>
+            </div>
+            <div class="category--card margin--medium">
+                <img src="Images/quiz.jpg" />
+                <h3 class="category--title">Category</h3>
+            </div>
+            <div class="category--card margin--medium">
+                <img src="Images/quiz.jpg" />
+                <h3 class="category--title">Category</h3>
+            </div>
+            <div class="category--card margin--medium">
+                <img src="Images/quiz.jpg" />
+                <h3 class="category--title">Category</h3>
+            </div>
+        </div>
+        <div class="container__flex--center margin--medium">
+            <a href="Pages/quizcategory.html"><button class="btn btn--outline">View All</button></a>
+        </div>
+      </div>
+    </>
   )
 }
 
