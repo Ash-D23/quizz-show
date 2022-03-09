12e575153d58d091d2114f8b46b7c9552a97f386
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-03-09 16:53:54 +0530

    refactor: modularized Cateogory page

--- src/Pages/Singlecategory/SingleCategory.js
+++ src/Pages/Singlecategory/SingleCategory.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SingleQuiz from '../../Components/SingleQuiz/SingleQuiz';
 import './SingleCategory.css'
 
 function SingleCategory() {
@@ -6,45 +7,9 @@ function SingleCategory() {
     <div class="container--90 margin--large">
         <h2 class="text--center padding--large">Category Name</h2>
         <div class="container__flex--center container__flex--wrap padding--medium">
-            <div class="card margin--medium">
-                <div class="card__image--container">
-                    <img class="card__image " src="../Images/quiz.jpg" />
-                </div>
-                <div class="card__body background--light padding--medium">
-                    
-                    <div class="container__flex--spacebetween">
-                        <h2 class="card__title text--large">Quiz Name</h2>
-                        <a href="quizrules.html"><button class="btn btn--red">Play Now</button></a>
-                    </div>
-
-                </div> 
-            </div>     
-            <div class="card margin--medium">
-                <div class="card__image--container">
-                    <img class="card__image " src="../Images/quiz.jpg" />
-                </div>
-                <div class="card__body background--light padding--medium">
-                    
-                    <div class="container__flex--spacebetween">
-                        <h2 class="card__title text--large">Quiz Name</h2>
-                        <a href="quizrules.html"><button class="btn btn--red">Play Now</button></a>
-                    </div>
-
-                </div> 
-            </div>    
-            <div class="card margin--medium">
-                <div class="card__image--container">
-                    <img class="card__image " src="../Images/quiz.jpg" />
-                </div>
-                <div class="card__body background--light padding--medium">
-                    
-                    <div class="container__flex--spacebetween">
-                        <h2 class="card__title text--large">Quiz Name</h2>
-                        <a href="quizrules.html"><button class="btn btn--red">Play Now</button></a>
-                    </div>
-
-                </div> 
-            </div>         
+            <SingleQuiz />
+            <SingleQuiz />
+            <SingleQuiz />        
         </div>  
     </div>
   )
